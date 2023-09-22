32bd7bcd774c134790bda63441e1d6f9db3d30fc
Aleksey <easy.mail.to.copy@gmail.com>
2023-09-22 21:26:27 +0300

    refactor: remove fixed card sizes

--- src/components/ui/card/card.stories.tsx
+++ src/components/ui/card/card.stories.tsx
@@ -12,5 +12,10 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    style: {
+      width: '420px',
+      height: '552px',
+    },
+  },
 }
