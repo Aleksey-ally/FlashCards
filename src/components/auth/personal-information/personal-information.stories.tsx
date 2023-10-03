a02ce9e3217280ffb822045aa32465a2d3ea8476
Maksym <maksymsydorovych@gmail.com>
2023-10-03 22:17:56 +0200

    fix: PersonalInformation/ changeName StoryBook

--- src/components/auth/personal-information/personal-information.stories.tsx
+++ src/components/auth/personal-information/personal-information.stories.tsx
@@ -1,6 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, Story } from '@storybook/react'
 
 import { PersonalInformation } from './'
+import { useState } from 'react'
 
 const meta = {
   title: 'Profile/Personal information',
@@ -9,23 +10,26 @@ const meta = {
 } satisfies Meta<typeof PersonalInformation>
 
 export default meta
-type Story = StoryObj<typeof meta>
+export const Default: Story = () => {
+  const [name, setName] = useState('New Name')
 
-export const Default: Story = {
-  args: {
-    email: 'example@doemail.com',
-    avatar:
-        'https://fastly.picsum.photos/id/819/200/200.jpg?hmac=nCwO4yKGbs8354aS0yf974UlPFBF_gwUSNazar7yBhk',
-    name: 'New Name',
-    onAvatarChange: () => {
-      console.info('avatar changed')
-    },
-    onNameChange: () => {
-      console.info('name changed')
-    },
-    onLogout: () => {
-      console.info('logout')
-    },
-  },
+  const handleNameChange = (newName: string) => {
+    console.info('name changed to:', newName)
+    setName(newName)
+  }
+
+  return (
+    <PersonalInformation
+      email="example@doemail.com"
+      avatar="https://fastly.picsum.photos/id/819/200/200.jpg?hmac=nCwO4yKGbs8354aS0yf974UlPFBF_gwUSNazar7yBhk"
+      name={name}
+      onAvatarChange={() => {
+        console.info('avatar changed')
+      }}
+      onNameChange={handleNameChange}
+      onLogout={() => {
+        console.info('logout')
+      }}
+    />
+  )
 }
-
