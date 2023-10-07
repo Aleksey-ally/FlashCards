9c8e5c0b363901ae24ceacfb3d0685448cac0aa5
Maksym <maksymsydorovych@gmail.com>
2023-10-07 17:28:26 +0200

    feat: RadioGroup#/11 and RadioGroupWithControll#/22

---
+++ src/components/ui/radio-group/radio-group.stories.tsx
@@ -0,0 +1,34 @@
+import { RadioGroup } from '@/components/ui/radio-group/radio-group.tsx'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'Components/Radio Group',
+  component: RadioGroup,
+  tags: ['autodocs'],
+} satisfies Meta<typeof RadioGroup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    options: [
+      { title: 'Option One', value: 'one' },
+      { title: 'Option Two', value: 'two' },
+      { title: 'Option Three', value: 'three' },
+      { title: 'Option Four', value: 'four' },
+    ],
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    options: [
+      { title: 'Option One', value: 'one' },
+      { title: 'Option Two', value: 'two' },
+      { title: 'Option Three', value: 'three' },
+      { title: 'Option Four', value: 'four' },
+    ],
+    disabled: true,
+  },
+}
