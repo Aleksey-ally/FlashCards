c44b42e8384eb2f5a8127963f6d2bbc34e011544
Aleksey <easy.mail.to.copy@gmail.com>
2023-10-09 22:20:55 +0300

    feat: add button which create new deck via server#25

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,3 +1,6 @@
+import s from './decks.module.scss'
+
+import { Button } from '@/components/ui/button'
 import {
   Table,
   TableBody,
@@ -6,32 +9,45 @@ import {
   TableHeadCell,
   TableRow,
 } from '@/components/ui/table'
-import { useGetDecksQuery } from '@/services/base-api.ts'
+import { Typography } from '@/components/ui/typography'
+import { useCreateDeckMutation, useGetDecksQuery } from '@/services/base-api.ts'
 
 export const Decks = () => {
   const { data } = useGetDecksQuery()
+  const [createDeck] = useCreateDeckMutation()
+
+  const onClickAddNewDeckButton = () => {
+    createDeck({ name: 'Temp test deck' })
+  }
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableHeadCell>Name</TableHeadCell>
-          <TableHeadCell>Cards</TableHeadCell>
-          <TableHeadCell> Last Updated</TableHeadCell>
-          <TableHeadCell> Created by</TableHeadCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data?.items?.map(deck => (
-          <TableRow key={deck.id}>
-            <TableCell>{deck.name}</TableCell>
-            <TableCell>{deck.cardsCount}</TableCell>
-            <TableCell>{new Date(deck.updated).toLocaleDateString()}</TableCell>
-            <TableCell>{deck.author.name}</TableCell>
-            <TableCell>icons...</TableCell>
+    <div className={s.pageDeck}>
+      <Button className={s.button} onClick={onClickAddNewDeckButton}>
+        <Typography variant="subtitle2" as="span">
+          Add new Deck
+        </Typography>
+      </Button>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Cards</TableHeadCell>
+            <TableHeadCell> Last Updated</TableHeadCell>
+            <TableHeadCell> Created by</TableHeadCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {data?.items?.map(deck => (
+            <TableRow key={deck.id}>
+              <TableCell>{deck.name}</TableCell>
+              <TableCell>{deck.cardsCount}</TableCell>
+              <TableCell>{new Date(deck.updated).toLocaleDateString()}</TableCell>
+              <TableCell>{deck.author.name}</TableCell>
+              <TableCell>icons...</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
