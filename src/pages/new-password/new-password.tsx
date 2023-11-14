43661cc5d1d6f318b9931498ea2a366aa550e716
Aleksey <easy.mail.to.copy@gmail.com>
2023-11-14 22:51:39 +0300

    feat: add handling errors and notification success for forms using toastify#49

--- src/pages/new-password/new-password.tsx
+++ src/pages/new-password/new-password.tsx
@@ -1,7 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 import { NewPassword } from '@/components/auth'
 import { NewPasswordForm } from '@/components/schemes/use-new-password-scheme'
+import { errorOptions, successOptions } from '@/pages/utils/toastify-options/toastify-options.ts'
 import { useResetPasswordMutation } from '@/services/auth/auth.service'
 
 export const NewPasswordPage = () => {
@@ -17,7 +19,13 @@ export const NewPasswordPage = () => {
       body: {
         password,
       },
-    }).then(() => navigate('/'))
+    })
+      .unwrap()
+      .then(() => {
+        toast.success('Password reset successfully', successOptions)
+        navigate('/')
+      })
+      .catch(() => toast.error('Incorrect or expired password reset token', errorOptions))
   }
 
   return <NewPassword onSubmit={resetPasswordHandler} />
