7e2fdde2fbe8e7823a37e7e40a7cf6f0437f7b8d
Maksym <maksymsydorovych@gmail.com>
2023-11-16 22:18:49 +0100

    feat: Loader

---
+++ src/components/ui/loader/loader.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Loader } from '@/components/ui/loader/loader.tsx'
+
+const meta = {
+  title: 'Components/Loader',
+  component: Loader,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Loader>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  render: () => <Loader />,
+}
