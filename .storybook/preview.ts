f4b90addb1fc271020497655c2e4cee499b9d5c8
Aleksey <easy.mail.to.copy@gmail.com>
2023-11-19 21:01:22 +0300

    feat: installed storybook-addon-react-router-v6 for using React Router in stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,8 +4,10 @@ import '@fontsource/roboto/700.css'
 import '../src/styles/index.scss'
 
 import type { Preview } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 const preview: Preview = {
+  decorators: [withRouter],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
