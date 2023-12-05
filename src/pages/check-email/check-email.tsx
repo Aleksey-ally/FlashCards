2f5c229b55d61fa69cc3b4690964a1c514704150
Aleksey <easy.mail.to.copy@gmail.com>
2023-12-05 22:12:05 +0300

    feat: add loader for all requests#50

--- src/pages/check-email/check-email.tsx
+++ src/pages/check-email/check-email.tsx
@@ -1,14 +1,20 @@
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { CheckEmail } from '@/components/auth'
+import { Loader } from '@/components/ui/loader'
+import { useMeQuery } from '@/services/auth/auth.service.ts'
 
 export const CheckEmailPage = () => {
+  const { isLoading } = useMeQuery()
+
   const { email } = useParams()
   const navigate = useNavigate()
 
   const onClickBackToSignIn = () => {
     navigate('/login')
   }
+
+  if (isLoading) return <Loader />
 
   return <CheckEmail onClick={onClickBackToSignIn} email={email} />
 }
